f9097b89b339f21296e9787d8421bf473cd70f83
Rob Fowler <rfo@mianos.com>
2020-08-22 12:26:30 +1000

    Pre python 3

--- clock.py
+++ clock.py
@@ -6,11 +6,13 @@ import requests
 import re
 import logging
 
+from influxdb import InfluxDBClient
+
 import gevent
 from gevent.pywsgi import WSGIServer
 
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, g
 from bs4 import BeautifulSoup
 
 # from flask.logging import default_handler
@@ -30,6 +32,10 @@ app = YourFlask(__name__)
 app.config['DEBUG'] = False
 #app.logger.removeHandler(default_handler)
 
+iclient = InfluxDBClient(host='mqtt2.mianos.com', port=8086)
+iclient.switch_database('radiation')
+
+
 @app.route('/')
 def hello_world():
     return render_template('clock.html')
@@ -37,10 +43,11 @@ def hello_world():
 
 last_request = arrow.Arrow.min
 wdata = None
+pm25 = 0
 
 @app.route('/time')
 def time():
-    global last_request, wdata
+    global last_request, wdata, pm25
 
     now = arrow.now()
     timestr = now.format('h:mm')
@@ -50,6 +57,8 @@ def time():
             tt = timestr
     if (now - last_request).seconds > 60 * 15:
         wdata = requests.get("http://rss.weatherzone.com.au/?u=12994-1285&lt=aploc&lc=624&obs=1&fc=1").text
+        aa = iclient.query('select last(datetime),pm25  from environment group by *');
+        pm25 = aa.raw['series'][0]['values'][0][2]
         last_request = now
 
     items = BeautifulSoup(wdata, 'html.parser').findAll('item')
@@ -62,9 +71,11 @@ def time():
     return jsonify(time=tt,
                    date="%s (sk %s)" % (now.strftime('%d %b'), arrow.now("Europe/Bratislava").format("h:mma")),
                    temp=temp_line,
-                   forecast=forecast_line)
+                   forecast=forecast_line,
+                   pm25="pm25: %d" % pm25)
 
 
 
 if __name__ == '__main__':
-    http_server = wsgi.WSGIServer(('', 5000), app, log=None).serve_forever()
+    # http_server = wsgi.WSGIServer(('', 5000), app, log=None).serve_forever()
+    http_server = WSGIServer(('', 5000), app, log=None).serve_forever()
